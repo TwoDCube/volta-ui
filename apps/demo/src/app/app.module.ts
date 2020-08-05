020d49c0331874a5771f2ce7ce82020a9f807775
TheEnd023 <sztibi023@gmail.com>
2020-08-05 17:52:50 +0200

    Setup angular library

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ButtonModule } from '@volta/ui';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    ButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
